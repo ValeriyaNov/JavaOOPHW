cabb4e81c1340c71394e6687e66fac0c88cb21a4
Valeriya <wal.now@yandex.ru>
2023-02-15 23:06:59 +0300

    1

--- HotDrinkVedingMachine.java
+++ HotDrinkVedingMachine.java
@@ -5,18 +5,17 @@ import java.util.ArrayList;
 public class HotDrinkVedingMachine extends WendingMachine{
     private ArrayList<Product> lst;
 
-    //protected int temperature;
     public HotDrinkVedingMachine(ArrayList<Product> lst) {
         super(lst);
         this.lst = lst;
-        //this.temperature = temperature;
+        
     }
 
     public HotDrink getProduct(String name, int volume, int temperature){
         for (Product product : lst){
         if (product instanceof HotDrink){ 
             HotDrink ht = (HotDrink) product;
-            //ht.printHotDrink();
+
             if (((ht.getName()).equals(name)) && (ht.getVolume() == volume) && (ht.getTemperature() == temperature)){
               return ht;  
             }
